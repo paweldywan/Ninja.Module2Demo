73f2f0d080548da7ae750326f5716d69f512f775
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-28 22:58:11 +0200

    Fix

--- NinjaDomain.Classes/Ninja.cs
+++ NinjaDomain.Classes/Ninja.cs
@@ -1,6 +1,7 @@
 ﻿using NinjaDomain.Classes.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NinjaDomain.Classes
 {
@@ -12,16 +13,28 @@ namespace NinjaDomain.Classes
         }
 
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
+
         public bool ServedInOniwaban { get; set; }
+
         public Clan Clan { get; set; }
+
         public int ClanId { get; set; }
+
         //Virtual umożliwia Lazy Loading
         public List<NinjaEquipment> EquipmentOwned { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
 
+
         public DateTime DateModified { get; set; }
+
         public DateTime DateCreated { get; set; }
+
         public bool IsDirty { get; set; }
     }
 }
---
+++ NinjaDomain.DataModel/Migrations/202007281946232_StringLengthForName.cs
@@ -0,0 +1,18 @@
+﻿namespace NinjaDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StringLengthForName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Ninjas", "Name", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Ninjas", "Name", c => c.String());
+        }
+    }
+}
